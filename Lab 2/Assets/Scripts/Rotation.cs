dbab63c3795ec3e2c823ec5ae15930ff6b02fb26
Martín España <martineduardoer00@gmail.com>
2020-04-05 20:33:00 -0600

    Se agregó el proyecto a GitHub

---
+++ Lab 2/Assets/Scripts/Rotation.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotation : MonoBehaviour
+{
+
+    private float velocity = 100;
+
+    public GameObject prefab;
+    private GameObject newObj;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(new Vector3(0, velocity, 0) * Time.deltaTime);
+
+        if (Input.GetButtonDown("Submit") && prefab && !newObj)
+        {
+            newObj = Instantiate<GameObject>(prefab, new Vector3(-9.28f, 1.39f, -10.63f), Quaternion.identity);
+        }
+    }
+}
